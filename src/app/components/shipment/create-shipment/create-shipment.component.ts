a21e12484fbfbf28ab9fa7412edef0da3ba049ef
Siew Wing Fei <siewwingfei@hotmail.com>
2019-11-18 15:06:01 +0800

    9

--- src/app/components/shipment/create-shipment/create-shipment.component.ts
+++ src/app/components/shipment/create-shipment/create-shipment.component.ts
@@ -557,7 +557,7 @@ export class CreateShipmentComponent implements OnInit {
     }
     
     else {
-      o.id = this.id;
+      o.id = Number(this.id);
       this.shipmentService.updateShipment(o).subscribe(res => {
         this.isloading = false;
         this.toastr.success('Shipment successfully updated', 'Update Shipment');
